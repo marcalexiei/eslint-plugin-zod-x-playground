3d453ae9900cde38e9e90241863b4423429d8307
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-09-17 13:43:39 +0200

    ci: enable

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
-import eslintPluginZodX from "eslint-plugin-zod-x";
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintPluginZodX from 'eslint-plugin-zod-x';
 
 export default defineConfig(
   eslint.configs.recommended,
@@ -11,11 +11,11 @@ export default defineConfig(
     languageOptions: {
       parserOptions: {
         projectService: {
-          allowDefaultProject: ["eslint.config.js"],
+          allowDefaultProject: ['eslint.config.js'],
         },
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  }
+  },
 );
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,26 +1,26 @@
-import { describe, it } from "node:test";
-import assert from "node:assert/strict";
+import { describe, it } from 'node:test';
+import assert from 'node:assert/strict';
 
-import { ESLint } from "eslint";
+import { ESLint } from 'eslint';
 
 const eslint = new ESLint();
 
-await describe("ESLint", () => {
+await describe('ESLint', () => {
   // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  it("should lint files in ./fixtures/generic without errors", async () => {
-    const result = await eslint.lintFiles(["./src/index.ts"]);
+  it('should lint files in ./fixtures/generic without errors', async () => {
+    const result = await eslint.lintFiles(['./src/index.ts']);
 
     assert.equal(result[0]?.errorCount, 6);
     assert.deepEqual(
       result[0]?.messages.map((m) => m.ruleId),
       [
-        "zod-x/prefer-namespace-import",
-        "zod-x/array-style",
-        "zod-x/no-throw-in-refine",
-        "zod-x/no-any",
-        "zod-x/prefer-meta",
-        "zod-x/prefer-meta-last",
-      ]
+        'zod-x/prefer-namespace-import',
+        'zod-x/array-style',
+        'zod-x/no-throw-in-refine',
+        'zod-x/no-any',
+        'zod-x/prefer-meta',
+        'zod-x/prefer-meta-last',
+      ],
     );
   });
 });
